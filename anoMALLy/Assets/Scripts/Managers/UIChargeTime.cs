e15581c455520de762d4ee056c4c4c63757fff5d
danielromeuperez-commits <danielromeuperez@gmail.com>
2026-05-22 18:39:19 +0200

    DELAY CARGA ANOMALIAS

--- anoMALLy/Assets/Scripts/Managers/UIChargeTime.cs
+++ anoMALLy/Assets/Scripts/Managers/UIChargeTime.cs
@@ -5,22 +5,60 @@ public class UIChargeTime : MonoBehaviour
 {
     [SerializeField] private Image chargedDetectorFill;
     [SerializeField] private float smoothSpeed = 5f;
+    [SerializeField] private float delayBeforeCharge = 0.5f;
 
     [SerializeField] private AnomalyDetector anodetector;
+
+    private float delayTimer;
+    private bool startedCharging;
 
     void Update()
     {
         if (chargedDetectorFill == null || anodetector == null)
             return;
 
-        float targetFill =
-            anodetector.CurrentFixTime /
-            anodetector.TimeToFix;
+        // Está cargando
+        if (anodetector.CurrentFixTime > 0f)
+        {
+            // Espera inicial
+            if (!startedCharging)
+            {
+                delayTimer += Time.deltaTime;
 
-        chargedDetectorFill.fillAmount = Mathf.Lerp(
-            chargedDetectorFill.fillAmount,
-            targetFill,
-            Time.deltaTime * smoothSpeed
-        );
+                chargedDetectorFill.fillAmount = 0f;
+
+                if (delayTimer >= delayBeforeCharge)
+                {
+                    startedCharging = true;
+                }
+
+                return;
+            }
+
+            // Progreso visual desde 0
+            float visualProgress =
+                (anodetector.CurrentFixTime - delayBeforeCharge) /
+                (anodetector.TimeToFix - delayBeforeCharge);
+
+            visualProgress = Mathf.Clamp01(visualProgress);
+
+            chargedDetectorFill.fillAmount = Mathf.Lerp(
+                chargedDetectorFill.fillAmount,
+                visualProgress,
+                Time.deltaTime * smoothSpeed
+            );
+        }
+        else
+        {
+            // Reset
+            delayTimer = 0f;
+            startedCharging = false;
+
+            chargedDetectorFill.fillAmount = Mathf.Lerp(
+                chargedDetectorFill.fillAmount,
+                0f,
+                Time.deltaTime * smoothSpeed
+            );
+        }
     }
 }
